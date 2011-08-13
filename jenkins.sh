a4aff1ce508493c27e37f3581f7efa840aa5370e
Oisin Mulvihill <oisin do-t mulvihill a-t gmail d-ot com>
2011-08-13 18:11:58 +0100

    adding jenkins script to test and report for CI purposes

---
+++ jenkins.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+TMPBUILDDIR=${TMPBUILDDIR:=/tmp/p_`date +'%Y%m%d%H%M%S'`}
+BASKET=${BASKET:=-i http://localhost/basket}
+SRC=`pwd`/lib
+
+# Build Test Environment:
+echo "Building Test Environment"
+virtualenv $TMPBUILDDIR
+source ${TMPBUILDDIR}/bin/activate
+${TMPBUILDDIR}/bin/easy_install ${BASKET} nose NoseXUnit coverage pylint
+
+if [ "$?" == 0 ]
+then
+    ${TMPBUILDDIR}/bin/nosetests -sv --with-nosexunit --source-folder=$SRC
+    #  --enable-cover --enable-audit
+else
+    echo "Test dependancy install FAILED!"
+fi
+
+# Clean up
+deactivate
+echo "Clean up: "${TMPBUILDDIR}
+rm -rf ${TMPBUILDDIR}
