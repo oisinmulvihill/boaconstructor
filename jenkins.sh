7264e1d7dcae70ceeeab77589a0bd338ce06fb35
Oisin Mulvihill <oisin do-t mulvihill a-t gmail d-ot com>
2011-08-13 18:20:34 +0100

    adding jenkins script to test and report for CI purposes

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #
-VIRTUALENV={VIRTUALENV:=virtualenv}
+VIRTUALENV=${VIRTUALENV:=virtualenv}
 TMPBUILDDIR=${TMPBUILDDIR:=/tmp/p_`date +'%Y%m%d%H%M%S'`}
 BASKET=${BASKET:=-i http://localhost/basket}
 SRC=`pwd`/lib
